4a6596cdbd9fe0ef871c0ad3ce1d72e3d0ada5cd
yetzt <git@yetzt.me>
2018-10-10 16:39:25 +0200

    set path to config file on cli

--- bin/tileblaster.js
+++ bin/tileblaster.js
@@ -1,3 +1,12 @@
 #!/usr/bin/env node
-require("../lib/tileblaster.js")(require("../config.js")).server().listen();
+require("../lib/tileblaster.js")(
+	(function(){
+		try {
+			return (!!process.argv[2]) ? require(require("path").resolve(process.cwd(), process.argv[2])) : require("../config.js");
+		} catch (err) {
+			console.error("usage: tileblaster <config.js>");
+			process.exit();
+		}
+	})()
+).server().listen();
 
