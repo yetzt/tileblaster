95a661e92f1ba2d9ffd0b12b4db4620aaeb0fac6
yetzt <git@yetzt.me>
2020-05-28 14:53:53 +0200

    add config options for queue and id, expose config options on cli

--- bin/tileblaster.js
+++ bin/tileblaster.js
@@ -1,12 +1,18 @@
 #!/usr/bin/env node
+var argv = require('minimist')(process.argv.slice(2));
 require("../lib/tileblaster.js")(
-	(function(){
+	(function(config){
+		["socket","tiles","queue","id"].forEach(function(n){
+			if (!!argv[n]) config[n] = argv[n];
+		});
+		return config;
+	})((function(){
 		try {
 			return (!!process.argv[2]) ? require(require("path").resolve(process.cwd(), process.argv[2])) : require("../config.js");
 		} catch (err) {
-			console.error("usage: tileblaster <config.js>");
-			process.exit();
+			console.error("usage: tileblaster <config.js> [--socket tileblaster.sock] [--tiles /path/to/tiles] [--queue 100] [--id mytileblaster]");
+			process.exit(1);
 		}
-	})()
+	})())
 ).server().listen();
 
