3d093edcdf7f0fa323a64559bebae933cbca4ecf
yetzt <git@yetzt.me>
2021-02-07 23:22:39 +0100

    reload config on file change

--- bin/tileblaster.js
+++ bin/tileblaster.js
@@ -1,14 +1,25 @@
 #!/usr/bin/env node
 var argv = require('minimist')(process.argv.slice(2));
-require("../lib/tileblaster.js")(
+var watch = require('node-watch');
+var configfile = require("path").resolve.apply(global, (!!argv._[0]) ? [ process.cwd(), argv._[0] ] : [ "../config.js" ]);
+var tb = require("../lib/tileblaster.js")(
 	(function(config){
 		["socket","tiles","queue","id"].forEach(function(n){
 			if (!!argv[n]) config[n] = argv[n];
 		});
+		// watch changes in config file
+		watch(configfile, function(evt,f){
+			if (evt === "update") try {
+				delete require.cache[require.resolve(configfile)];
+				tb.reconfigure(require(configfile));
+			} catch (err) {
+				console.error("Unable to read changed config file", err);
+			}
+		});
 		return config;
 	})((function(){
 		try {
-			return (!!process.argv[2]) ? require(require("path").resolve(process.cwd(), process.argv[2])) : require("../config.js");
+			return require(configfile);
 		} catch (err) {
 			console.error("usage: tileblaster <config.js> [--socket tileblaster.sock] [--tiles /path/to/tiles] [--queue 100] [--id mytileblaster]");
 			process.exit(1);
