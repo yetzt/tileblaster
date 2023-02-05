8de9cc60e7a75ae3ecfcbb35d4c4aa6c7de486d7
yetzt <git@yetzt.me>
2023-02-05 03:01:28 +0100

    add debug

--- tileblaster.js
+++ tileblaster.js
@@ -1,4 +1,6 @@
 
+const debug = require("./lib/debug")("tileblaster");
+
 const tileblaster = module.exports = function tileblaster(config){
 	if (!(this instanceof tileblaster)) return new tileblaster(...arguments);
 	const self = this;
@@ -9,6 +11,8 @@ const tileblaster = module.exports = function tileblaster(config){
 
 // run if called directly
 if (require.main === module) {
+	process.env.DEBUG = process.env.DEBUG || "tileblaster";
+	debug("starting tileblaster");
 	tileblaster({
 		port: 8080,
 		...require("./config"),
