acff666c52b0b32e738a1d082c61cce5df085fe2
yetzt <git@yetzt.me>
2023-02-05 03:03:37 +0100

    implement server

--- tileblaster.js
+++ tileblaster.js
@@ -1,10 +1,18 @@
 
+const http = require("http");
+const fs = require("fs");
+
 const router = require("./lib/router");
 const debug = require("./lib/debug")("tileblaster");
 
 const tileblaster = module.exports = function tileblaster(config){
 	if (!(this instanceof tileblaster)) return new tileblaster(...arguments);
 	const self = this;
+
+	self.config = config;
+
+	// default host to localhost if port is set
+	if (self.config.port && !self.config.host) self.config.host = "localhost";
 
 	// router
 	self.router = router({ mountpoint: "/" }); // FIXME mountpoint from config
@@ -23,8 +31,44 @@ const tileblaster = module.exports = function tileblaster(config){
 		res.end("not found.");
 	});
 
+	// listen
+	self.listen(self.router.serve);
 
 	return this;
+};
+
+// listen handler
+tileblaster.prototype.listen = function(router){
+	const self = this;
+
+	// listen on port
+	if (self.config.port) {
+		http.createServer(function(){
+			router(...arguments);
+		}).listen(self.config.port, self.config.host, function(err){
+			if (err) return debug("listen: ERROR binding port '%s:%d':", self.config.host, self.config.port, err);
+			debug("listen: listening on '%s:%d'", self.config.host, self.config.port);
+		});
+	};
+
+	// listen on socket
+	if (self.config.socket) {
+		fs.unlink(self.config.socket, function(err) { // try unlink leftover socket
+			if (err && err.code !== "ENOENT") return debug("listen: ERROR deleting socket '%s':", self.config.socket, err);
+			http.createServer(function(){
+				router(...arguments);
+			}).listen(self.config.socket, function(err) {
+				if (err) return debug("listen: ERROR binding to socket '%s':", self.config.socket, err);
+				debug("listen: listening on socket '%s'", self.config.socket);
+				if (self.config.perms) fs.chmod(self.config.socket, self.config.perms, function(err){
+					if (err) return debug("listen: ERROR changing permissions of socket '%s' to '%s':", self.config.perms.toString(8), self.config.socket, err);
+					// FIXME chgrp
+				});
+			});
+		});
+	}
+
+	return self;
 };
 
 // run if called directly
