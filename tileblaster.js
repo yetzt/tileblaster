44eac2393e75f8aa51e2bb8e0cf1f459028b572a
yetzt <git@yetzt.me>
2023-02-05 03:02:40 +0100

    add router

--- tileblaster.js
+++ tileblaster.js
@@ -1,9 +1,27 @@
 
+const router = require("./lib/router");
 const debug = require("./lib/debug")("tileblaster");
 
 const tileblaster = module.exports = function tileblaster(config){
 	if (!(this instanceof tileblaster)) return new tileblaster(...arguments);
 	const self = this;
+
+	// router
+	self.router = router({ mountpoint: "/" }); // FIXME mountpoint from config
+
+	// index route
+	self.router.route("/", function(req, res){
+		res.statusCode = 200;
+		res.setHeader("content-type", "text/plain");
+		res.end("tileblaster ready.");
+	});
+
+	// index route
+	self.router.default(function(req, res){
+		res.statusCode = 404;
+		res.setHeader("content-type", "text/plain");
+		res.end("not found.");
+	});
 
 
 	return this;
