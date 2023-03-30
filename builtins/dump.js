9319c0aaae6e8b23b429446d4fde73e8c95920e7
yetzt <git@yetzt.me>
2023-03-30 03:13:35 +0200

    format, use skip()

--- builtins/dump.js
+++ builtins/dump.js
@@ -1,16 +1,19 @@
 const util = require("util");
 
 // a debuging module: dump all the information and end the request
-module.exports = function({ res, data }, next){
+module.exports = function({ res, data }, next, skip){
 	const dump = util.inspect(data, false, 3);
 
 	console.error(dump);
 
-	// do nothing if response stream has been uses
+	// do nothing if response stream has been used
 	if (res.used || res.headersSent || !res.writable || res.destroyed || res.finished || res.closed || res.piped) return;
 
 	res.statusCode = 200;
 	res.setHeader("content-type", "text/plain");
 	res.end(dump);
+
+	res.used = true;
+	skip();
 
 };
