48e26d8f7191b844deb50f90430db68ffc8c3ca5
yetzt <git@yetzt.me>
2023-08-23 16:46:58 +0200

    fail gracefully

--- builtins/versatiles.js
+++ builtins/versatiles.js
@@ -4,7 +4,7 @@ const versatiles = load("versatiles");
 const cache = {};
 
 // versatiles backend
-module.exports = function({ req, res, opts, data }, next){
+module.exports = function({ req, res, opts, data }, next, skip){
 	const mime = this.lib.mime;
 	const debug = this.lib.debug;
 
@@ -16,12 +16,22 @@ module.exports = function({ req, res, opts, data }, next){
 			tms: (!!opts.tms),
 			headers: ((opts.hasOwnProperty("headers")) ? opts.headers : {}),
 		});
+
+		// skip function
+		cache[data.map].abort = function(err){
+			res.statusCode = 204; // no content
+			res.setHeader("x-tileblaster-hint", err.message || err.toString());
+			res.end();
+			res.used = true; // mark connection as used
+			return skip(); // skip rest of jobs
+		};
+
 	};
 	const vt = cache[data.map];
 
 	debug.info("Fetching %s/%s/%s", data.req.params.z, data.req.params.x, data.req.params.y);
 	vt.getTile(data.req.params.z, data.req.params.x, data.req.params.y, function(err, buf){
-		if (err) return next(err);
+		if (err) return cache[data.map].abort(err); // fail gracefully
 
 		// if precompressed, keep in tile stack
 		/* TODO evaluate side effects
@@ -40,7 +50,7 @@ module.exports = function({ req, res, opts, data }, next){
 
 		// decompress tile
 		vt.decompress(vt.header.tile_precompression, buf, function(err, buf){
-			if (err) return next(err);
+			if (err) return cache[data.map].abort(err); // fail gracefully
 
 			const tile = {
 				buffer: buf,
